d62538ddac52a03a80edee13d81fa1bf398ada73
Your Name <you@example.com>
2019-06-05 13:56:19 +0800

    第一次提交

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/123")
+    @ResponseBody
+    public String getSuperSpeciesIndustryList(HttpServletRequest request, HttpServletResponse response){
+
+        System.out.print("ascvavmd");
+        return "abc";
+    }
+}
